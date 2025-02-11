67c4d225649aad2345871e9face20b3392f6271c
giorgiospirostopala <gio.topala@gmail.com>
2025-02-12 00:54:04 +0100

    add: scripts + res test

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
 // Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità sulla base di quello che impareremo.
 
-//- Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+// Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
 
-// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+//- Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 
 // Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
 
